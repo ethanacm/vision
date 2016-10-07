7a41eb000b7a82ceec8eb21ed8e9ac46494c9630
Omar Shehata <omar.sameh.shehata@gmail.com>
2016-10-06 22:11:18 -0500

    Added SimpleCV hello world example

---
+++ hello_world.py
@@ -0,0 +1,16 @@
+# Grabs a screenshot of your webcam and displays it
+
+from SimpleCV import Camera, Display, Image
+import time
+# Initialize the camera
+cam = Camera()
+# Initialize the display
+display = Display()
+# Snap a picture using the camera
+img = cam.getImage()
+# Show some text
+img.drawText("Hello World!")
+# Show the picture on the screen
+img.save(display)
+# Wait five seconds so the window doesn't close right away
+time.sleep(5)
